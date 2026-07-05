41c653456c11d2538c3fd5bb20f63fa621d877e3
Harsh More <moreharsh2001@gmail.com>
2026-07-05 18:18:50 -0400

    Port updated

--- backend/main.py
+++ backend/main.py
@@ -219,4 +219,5 @@ async def get_menu_dashboard(request: MenuDashboardRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
